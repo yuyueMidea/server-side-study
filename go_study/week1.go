e64ad7a07396c09f7a927538a130a4bd2081b07e
苍穹勇敢的心 <18825237023@163.com>
2025-09-27 21:50:15 +0800

    新增：数据类型练习

--- go_study/week1.go
+++ go_study/week1.go
@@ -18,5 +18,22 @@ func main() {
 	height += 2
 	const pi = 3.14
 	res1 := sumN(3)
-	fmt.Println("name: ", name, "msg: ", msg, ", rs1: ", res1, "height: ", height, ", pi : ", pi)
+	fmt.Println("name: ", name, "msg: ", msg, ", rs1: ", res1, "height: ", height, ", pi : ", pi, "======\n=====")
+
+	// 基本数据类型
+	var b bool = true
+	var i int = 21
+	var f float64 = 3.14
+	var s string = "str123"
+	// 数组---切片
+	var arr [3]int = [3]int{1, 2, 3}
+	var slice = []string{"apple", "orange", "banana"}
+	// 映射
+	m := make(map[string]int)
+	m["go"] = 2009
+	m["java"] = 1995
+
+	fmt.Printf("布尔: %v, 整数: %v, 浮点: %v, 字符串: %v\n", b, i, f, s)
+	fmt.Printf("数组: %v, 切片: %v, 映射: %v\n", arr, slice, m)
+
 }
