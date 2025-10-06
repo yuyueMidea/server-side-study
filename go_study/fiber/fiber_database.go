4ef66a2ca086c73223a1f90a2235b2124765effa
苍穹勇敢的心 <18825237023@163.com>
2025-10-06 15:04:27 +0800

    fiber学习，添加：路由与参数（Path、Query、Body）

--- go_study/fiber/fiber_database.go
+++ go_study/fiber/fiber_database.go
@@ -13,5 +13,15 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("hello, go fiber!")
 	})
+	// 2) 路由与参数（Path、Query、Body）
+	app.Get("/hello/:name", func(c *fiber.Ctx) error {
+		name := c.Params("name")
+		lang := c.Query("lang", "zh-CN")
+		fmt.Println("nnn: ", name)
+		return c.JSON(fiber.Map{
+			"msg":  "hi, my name is " + name,
+			"lang": lang,
+		})
+	})
 	log.Fatal(app.Listen(":8080"))
 }
