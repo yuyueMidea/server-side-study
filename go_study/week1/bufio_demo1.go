22a1b334cce7cf365c455623fb85a123ccc9237f
苍穹勇敢的心 <18825237023@163.com>
2025-10-09 16:53:12 +0800

    使用go读文件，字数、行数统计、byte统计等

---
+++ go_study/week1/bufio_demo1.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func count(path string) (lines, words, bytes int, err error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	sc := bufio.NewScanner(f)
+	sc.Split(bufio.ScanLines)
+	for sc.Scan() {
+		lines++
+		line := sc.Text()
+		bytes += len(line) + 1 // 简化：含换行
+		ws := bufio.NewScanner(strings.NewReader(line))
+		ws.Split(bufio.ScanWords)
+		for ws.Scan() {
+			words++
+		}
+	}
+	return lines, words, bytes, sc.Err()
+}
+func main() {
+	println("===== read file and count ====")
+	path := flag.String("f", "", "file path")
+	flag.Parse()
+	if *path == "" {
+		fmt.Fprintln(os.Stderr, "usage: wc -f <file>")
+		os.Exit(2)
+	}
+	l, w, b, err := count(*path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "count error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("lines=%d words=%d bytes=%d\n", l, w, b)
+}
